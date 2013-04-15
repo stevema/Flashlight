4f517491e966d38488b7c35c6c98e78594ba3155
steve <chunlinpage@gmail.com>
2013-04-15 17:57:17 +0800

    update

--- Light/AppDelegate.m
+++ Light/AppDelegate.m
@@ -11,15 +11,51 @@
 #import "ViewController.h"
 
 @implementation AppDelegate
+{
+    BOOL torchIsOn;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-    self.window.rootViewController = self.viewController;
+    //    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    //    self.window.rootViewController = self.viewController;
+    self.window.backgroundColor = [UIColor blackColor];
+	// Do any additional setup after loading the view, typically from a nib.
+    _lightView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 20, 320, 320)];
+    _lightView.image = [UIImage imageNamed:@"nomorl"];
+    torchIsOn = NO;
+    [self Touch:self];
+    _lightView.userInteractionEnabled = YES;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(Touch:)];
+    [_lightView addGestureRecognizer:tap];
+    [self.window addSubview:_lightView];
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (IBAction)Touch:(id)sender {
+    Class captureDeviceClass = NSClassFromString(@"AVCaptureDevice");
+    if (captureDeviceClass != nil) {
+        AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        if ([device hasTorch] && [device hasFlash]){
+            
+            [device lockForConfiguration:nil];
+            if (!torchIsOn) {
+                [device setTorchMode:AVCaptureTorchModeOn];
+                [device setFlashMode:AVCaptureFlashModeOn];
+                _lightView.image = [UIImage imageNamed:@"selected"];
+                torchIsOn = YES;
+            } else {
+                [device setTorchMode:AVCaptureTorchModeOff];
+                [device setFlashMode:AVCaptureFlashModeOff];
+                _lightView.image = [UIImage imageNamed:@"nomorl"];
+                torchIsOn = NO;
+            }
+            [device unlockForConfiguration];
+        }
+    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
@@ -30,7 +66,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
@@ -42,6 +78,8 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    torchIsOn = NO;
+    [self Touch:self];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
